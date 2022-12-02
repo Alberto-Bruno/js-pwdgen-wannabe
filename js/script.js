7492865b52432109672620a0b07253af0e34d19f
Alberto-Bruno <brunoalberto97@outlook.it>
2022-12-02 17:42:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('JS OK');
+
+/*
+1-Chiedere all'utente il suo nome
+2-Chiedi il suo cognome
+3-Chiedi il suo colore preferito
+5-Scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+
+// Chiedo all'utente il suo nome
+const firstName = prompt('Come ti chiami?' , 'Albe');
+console.log(firstName);
+
+//Chiedo all'utente il suo cognome
+const lastName = prompt('Il suo cognome?' , 'Bruno');
+console.log(lastName);
